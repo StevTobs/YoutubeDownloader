ad3b33d940a27785eb2a6dd03b96eba3f0654652
Akanit Kwangkaew <akanitk84@gmail.com>
2022-08-28 01:14:00 +0700

    Add files via upload

---
+++ youtube_loader.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import filedialog
+
+screen = Tk()
+title = screen.title('Youtube Download')
+canvas = Canvas(screen, width = 500, height=500)
+canvas.pack()
+
+
+screen.mainloop()
+
+
+
